d77dcc230a0e0a940cf26d26b2d5bbaa51c44635
QiuDB <qiudb2018@outlook.com>
2019-08-01 10:24:03 +0800

    watch config path

--- game-server/app/utils/dataApi.js
+++ game-server/app/utils/dataApi.js
@@ -6,17 +6,35 @@ const pomelo = require('pomelo');
 const configPath = path.resolve(pomelo.app.getBase() + '/config/data');
 let DataApi = function() {
     this.datas = null;
-    let watcher = chokidar.watch(configPath);
-    watcher.on('change', function(path) {
-        logger.warn('File %j was changed', path);
+    this.watcher = null;
+};
+
+DataApi.prototype.init = function() {
+    let watcher = chokidar.watch(configPath, {
+        ignoreInitial: true
     });
 
-    watcher.on('add', function(path) {
-        logger.warn('File %j was added', path);
+    // 监听配置文件修改
+    watcher.on('change', (path) => {
+        this.watchFileChanged(path)
+    })
+
+    // 监听配置文件新增
+    watcher.on('add', (path) => {
+        this.watchFileAdded(path)
     });
+}
+
+DataApi.prototype.watchFileChanged = function(filepath) {
+    logger.warn('serverId %j, File %j was changed', pomelo.app.getServerId(), filepath);
 };
+
+DataApi.prototype.watchFileAdded = function(filepath) {
+    logger.warn('serverId %j, File %j was added', pomelo.app.getServerId(), filepath);
+}
 
 module.exports = {
     id: 'dataApi',
-    func: DataApi
+    func: DataApi,
+    init: 'init'
 }
