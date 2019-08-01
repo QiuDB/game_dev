488fc75516e75ae76e22348e40e8c3890a4bb972
QiuDB <qiudb2018@outlook.com>
2019-08-01 15:44:21 +0800

    charactor module

---
+++ game-server/app/domain/entity/charactor.js
@@ -0,0 +1,58 @@
+const bearcat = require('bearcat');
+const util = require('util');
+const logger = require('pomelo-logger').getLogger('pomelo', __filename);
+
+let Charactor = function(opts) {
+    let Entity = bearcat.getFunction('entity');
+    Entity.call(this, opts);
+
+    this.dataApi = null;
+
+    this.orientation = opts.orientation;
+    this.target = null;
+    this.attackers = {};
+
+    // the entity who hate me
+    // I would notify my enemies to forget me when I disapear or die
+    this.enemies = {};
+
+    // the entity I hate
+    // I would set my target as the entity that I hate most
+    this.haters = {};
+
+    this.died = false;
+    this.hp = opts.hp;
+    this.mp = opts.mp;
+    this.maxHp = opts.maxHp;
+    this.maxMp = opts.maxMp;
+    this.level = opts.level;
+    this.experience = opts.experience;
+    this.attackValue = opts.attackValue;
+    this.defenceValue = opts.defenceValue;
+    this.totalAttackValue = opts.totalAttackValue || 0;
+    this.totalDefenceValue = opts.totalDefenceValue || 0;
+    this.hitRate = opts.hitRate;
+    this.dodgeRate = opts.dodgeRate;
+    this.walkSpeed = opts.walkSpeed;
+    this.attackSpeed = opts.attackSpeed;
+    this.isMoving = false;
+
+    this.attackParam = 1;
+    this.defenceParam = 1;
+    this.equipmentParam = 1;
+    this.buffs = [];
+    this.curSkill = 1;  //default normal attack
+    this.characterData = this.dataApi.character().findById(this.kindId);
+    this.fightSkills = {};
+};
+
+module.exports = {
+    id: 'charactor',
+    func: Charactor,
+    scope: 'prototype',
+    parent: 'entity',
+    props: [{
+        name: 'dataApi',
+        ref: 'dataApi'
+    }]
+}
