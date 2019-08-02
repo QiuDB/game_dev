3cefc278ab28f2e018ad6f40f9a3de20dac2db35
QiuDB <qiudb2018@outlook.com>
2019-08-02 09:27:27 +0800

    add npcEvent

--- game-server/app/event/npcEvent.js
+++ game-server/app/event/npcEvent.js
@@ -3,3 +3,16 @@ const logger = require('pomelo-logger').getLogger('pomelo', __filename);
 /**
  * Handler npc event
  */
+let NpcEvent = function() {};
+
+NpcEvent.prototype.addEventForNpc = function(npc) {
+    /**Handle npc talk event */
+    npc.on('npcTalk', function(data) {
+        logger.debug('catch npcTalk event:\n %o', data);
+    });
+};
+
+module.exports = {
+    id: 'npcEvent',
+    func: NpcEvent
+}
